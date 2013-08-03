9d633f8e084b0e911f88313e5fdf891869c861b2
daclouds <daclouds@gmail.com>
2013-08-03 10:35:50 +0900

    added generic type paramter

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,9 +4,8 @@ import org.vertx.java.platform.Verticle;
 
 public class Server extends Verticle {
 		public void start() {
-				vertx.createHttpServer().requestHandler(new Handler() {
-						public void handle(Object obj) {
-							HttpServerRequest req = (HttpServerRequest) obj;
+				vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+						public void handle(HttpServerRequest req) {
 							String file = req.path().equals("/") ? "index.html" : req.path();
 							req.response().sendFile("webroot/" + file);
 						}
