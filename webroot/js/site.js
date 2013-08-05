7a2a1021ac7efef8ec7a8181c98b4316f65dbb89
Ham <hmkang85@gmail.com>
2013-08-05 23:35:42 +0900

    site module 적용

---
+++ webroot/js/site.js
@@ -0,0 +1,29 @@
+var Site = angular.module('Site', []);
+
+Site.config(function ($routeProvider) {
+	$routeProvider
+	//.when('/:slug', {templateUrl: 'main.html', controller: 'RouteController'})
+	.when('/main', {templateUrl: 'main.html', controller: 'RouteController'})
+	.when('/tree', {templateUrl: 'tree.html', controller: 'RouteController'})
+	.otherwise({redirectTo: '/main'});
+});
+
+function AppController ($scope, $rootScope, $http) {
+	// Load pages on startup
+	//$http.get('pages.json').success(function (data) {
+	//	$rootScope.pages = data;
+	//});
+
+	// Set the slug for menu active class
+	$scope.$on('routeLoaded', function (event, args) {
+		$scope.slug = args.slug;
+	});
+}
+
+function RouteController ($scope, $rootScope, $routeParams) {
+	// Getting the slug from $routeParams
+	var slug = $routeParams.slug;
+	
+	$scope.$emit('routeLoaded', {slug: slug});
+	$scope.page = $rootScope.pages[slug];
+}
