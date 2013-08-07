036288da913a1ca47056c862b4f53a8396aff62d
kang hwan ki <xangfi@gmail.com>
2013-08-07 11:35:32 +0900

    logger 추가. 소스원복

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,13 +2,31 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.platform.Verticle;
 
+import common.SampleLogger;
+
 public class Server extends Verticle {
+	private static final int 	LISTEN_PORT = 8080;
+	private SampleLogger 		log;
+
 	public void start() {
-		vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-			public void handle(HttpServerRequest req) {
-				String file = req.path().equals("/") ? "index.html" : req.path();
-				req.response().sendFile("webroot/" + file);
-			}
-		}).listen(8080);
+		log = new SampleLogger(getContainer());
+
+		Listener listener = new Listener();
+
+		vertx.createHttpServer().requestHandler(listener).listen(LISTEN_PORT);
+
+		log.info("start Server... \r\nlisten : %d", LISTEN_PORT);
+	}
+
+	private class Listener implements Handler<HttpServerRequest> {
+		@Override
+		public void handle(HttpServerRequest request) {
+			String path = request.path();
+			String file = path.equals("/") ? "index.html" : path;
+
+			log.info("file : %s", file);
+
+			request.response().sendFile("webroot/" + file);
+		}
 	}
 }
