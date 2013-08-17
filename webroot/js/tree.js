11ec0827b81b882182feb84eb334a78c37938411
daclouds <daclouds@gmail.com>
2013-08-18 08:52:35 +0900

    added view comments

--- webroot/js/tree.js
+++ webroot/js/tree.js
@@ -35,47 +35,40 @@ function treeCtrl($scope, $http) {
 				url: 'https://api.github.com/repos/'+$('#owner').val()+'/'+$('#repo').val()+'/git/blobs/' + tree.sha, 
 				method: 'GET', 
 					headers: {'Accept': 'application/vnd.github.VERSION.raw' }
-				}).success(function(data) {
+				}).success(function(code) {
 				
-					$('#content').empty();
+					$scope.comments = {};
+					$http({
+						url: '/json/comment_sample.json',
+						method: 'GET'
+					}).success(function(comments) {
+						$.each(comments, function(idx, comment) {
+							$scope.comments[comment.end_line] = comment;
+						});
 
-					var source = '';
-					var lines = data.split("\n");
+						$('#content').empty();
 
-					d = document.createElement('pre');
+						var source = '';
+						var lines = code.split("\n");
+						$scope.lines = lines;
 
-					// 라인 넘버 추가
-					for (var i = 0, j = lines.length; i < j; i++) {
-						var line = lines[i];
-						source += (i+1) + "\t" + line + "\n";
+						d = document.createElement('pre');
 
-						// review 의견 추가
-						if ((i+1) % 6 == 0) {
-							$(d).addClass('prettyprint');
-							$(d).text(source);
-							$(d).appendTo($('#content'));
-
-							d = document.createElement('div');
-
-							var review = '<div class="panel" style="width: 90%; margin-left: 40px">';
-							review += '별로~~';
-							review += '</div>';	// end of panel
-							$(d).html(review);
-							$(d).appendTo($('#content'));
-
-							d = document.createElement('pre');
-							source = '';
+						// 라인 넘버 추가
+						for (var i = 0, j = lines.length; i < j; i++) {
+							var line = lines[i];
+							source += (i+1) + "\t" + line + "\n";
 						}
 
 						// review 와 code 를 구분
 						$(d).addClass('prettyprint');
 						$(d).text(source);
 						$(d).appendTo($('#content'));
-					}
 
-					// css 를 다시 적용
-					$('.prettyprint').removeClass("prettyprinted");
-					prettyPrint();
+						// css 를 다시 적용
+						$('.prettyprint').removeClass("prettyprinted");
+						prettyPrint();
+					});
 			});
 
 		} else {
@@ -91,5 +84,72 @@ function treeCtrl($scope, $http) {
 			});
 
 		}
+	}
+
+	$scope.viewComment = function(end_line) {
+		$('#content').empty();
+
+		// temp
+		var comment = $scope.comments[end_line];
+		console.log(comment);
+
+		var source = '';
+		var lines = $scope.lines;
+
+		d = document.createElement('pre');
+
+		// 라인 넘버 추가
+		var line = '';
+		for (var i = 0, j = lines.length; i < j; i++) {
+			line = lines[i];
+			
+			// review 의견 추가
+			if (comment.start_line == i + 1) {
+
+				// 이전 내용을 저장
+				$(d).addClass('prettyprint');
+				$(d).text(source);
+				$(d).appendTo($('#content'));
+
+				source = '';
+
+				// review 블록 지정
+				for (j = comment.end_line; i < j; i++) {
+					line = lines[i];
+					source += (i+1) + "\t" + line + "\n";
+				}
+
+				d = document.createElement('pre');
+				$(d).addClass('prettyprint');
+				$(d).text(source);
+				$(d).appendTo($('#content'));
+
+				// review 내용 보기
+				d = document.createElement('div');
+
+				var review = '<div class="panel" style="width: 90%; margin-left: 40px">';
+				review += comment.comment;
+				review += '</div>';	// end of panel
+				$(d).html(review);
+				$(d).appendTo($('#content'));
+
+				d = document.createElement('pre');
+				source = '';
+
+				j = lines.length;
+			}
+
+			source += (i+1) + "\t" + line + "\n";
+		}
+
+		// review 와 code 를 구분
+		$(d).addClass('prettyprint');
+		$(d).text(source);
+		$(d).appendTo($('#content'));
+
+		// css 를 다시 적용
+		$('.prettyprint').removeClass("prettyprinted");
+		$('.prettyprint').css({ margin: "0px" });
+		prettyPrint();
 	}
 }
